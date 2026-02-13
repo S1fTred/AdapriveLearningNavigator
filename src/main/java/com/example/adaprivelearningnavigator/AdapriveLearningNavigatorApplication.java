e74a8a160e99505cadb612fd00d97b3cf72f4f71
S1fTred <matvei.tchufistov@gmail.com>
2026-02-13 13:56:59 +0300

    Initial commit

---
+++ src/main/java/com/example/adaprivelearningnavigator/AdapriveLearningNavigatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.adaprivelearningnavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapriveLearningNavigatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdapriveLearningNavigatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adaprivelearningnavigator/AdapriveLearningNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adaprivelearningnavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapriveLearningNavigatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
